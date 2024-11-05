3b10079ceaafb1622b783db058e4afb144897eda
shanmukhaReddyk <kethushanmukhareddy2126@gmail.com>
2024-11-05 16:31:12 +0530

    change user1 and user 2 by user 2

--- user1.cpp
+++ user1.cpp
@@ -4,6 +4,7 @@ using namespace std;
 int main(){
 
 	cout<<"user1 added this "<<endl;
+	cout<<"user2 added this";	
 	return 0;
 }
 
--- user2.cpp
+++ user2.cpp
@@ -4,5 +4,6 @@ using namespace std;
 int main(){
 
 	cout<<" user2 added this"<<endl;
+	cout<<" user 2 added another line";
 	return 0;
 }
